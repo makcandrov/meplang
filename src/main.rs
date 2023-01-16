ed6bcd0043b796e1b30d024f963bfeec832a252e
makcandrov <makcandrov@proton.me>
2023-01-16 03:32:23 -0800

    limprove .pest

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
 use std::collections::HashSet;
 
+use attribute::Attribute;
 use block::Block;
 
 mod attribute;
 mod block;
 mod variable;
-
-fn main() {
-    let code = std::fs::read_to_string("input.mep").unwrap();
-    let parsed = MeplangParser::parse(Rule::file, &code);
-    dbg!(parsed);
-}
-
-#[derive(Debug, Clone)]
-pub struct Contract (Vec<Block>);
-
-#[derive(Debug, Copy, Clone)]
-pub enum ParsingError {
-    Generic,
-}
 
 extern crate pest;
 #[macro_use]
@@ -28,3 +15,25 @@ use pest::Parser;
 #[derive(Parser)]
 #[grammar = "meplang.pest"]
 pub struct MeplangParser;
+
+fn main() {
+    let code = std::fs::read_to_string("input.mep").unwrap();
+    let parsed = match MeplangParser::parse(Rule::file, &code) {
+        Ok(parsed) => parsed,
+        Err(err) => {
+            println!("parsing failed: {}", err);
+            return;
+        },
+    };
+    // parsed.
+    // for item in parsed {
+    //     dbg!(item);
+    // }
+    dbg!(parsed);
+}
+
+pub struct Contract {
+    pub attributes: Vec<Attribute>,
+    pub blocks: Vec<Block>,
+}
+
